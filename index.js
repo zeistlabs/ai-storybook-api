696b7cddc1da0d8fbd1e6ce7ef8d85adf5f29c23
farshed <faisalarshed28@gmail.com>
2025-05-31 11:35:02 +0500

    Update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-require("./src/dbconfig/config");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+require('./src/dbconfig/config');
+const cors = require('cors');
+const path = require('path');
 
-const userRoutes = require("./src/routes/user_routes");
-const storyRoutes = require("./src/routes/generate_story_routes");
-const emailRoutes = require("./src/routes/email_routes");
+const userRoutes = require('./src/routes/user_routes');
+const storyRoutes = require('./src/routes/generate_story_routes');
+const emailRoutes = require('./src/routes/email_routes');
 
 const app = express();
 const PORT = 5005;
@@ -16,11 +16,11 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-app.use("/user", userRoutes);
-app.use("/story", storyRoutes);
+app.use('/user', userRoutes);
+app.use('/story', storyRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use("/api/contactus", emailRoutes)
+app.use('/api/contactus', emailRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
--- src/routes/email_routes.js
+++ src/routes/email_routes.js
@@ -1,26 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS
+	}
 });
 
-router.post("/", async (req, res) => {
-    const { lastName, firstName, email, message } = req.body;
+router.post('/', async (req, res) => {
+	const { lastName, firstName, email, message } = req.body;
 
-    const mailOptions = {
-        from: email,
-        to: process.env.EMAIL_USER,
-        subject: 'Nouveau message depuis le formulaire de contact',
-        html: `
+	const mailOptions = {
+		from: email,
+		to: process.env.EMAIL_USER,
+		subject: 'Nouveau message depuis le formulaire de contact',
+		html: `
           <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; border: 1px solid #eee; padding: 20px; border-radius: 10px; background-color: #f9f9f9;">
             <h2 style="color: #333; text-align: center;">📩 Nouveau Message de Contact</h2>
             <p><strong>👤 Nom:</strong> ${lastName}</p>
@@ -30,15 +27,15 @@ router.post("/", async (req, res) => {
             <p style="white-space: pre-line;"><strong>Message:</strong><br><div style="padding: 5px;">${message}</div></p>
           </div>
         `
-    };
+	};
 
-    try {
-        await transporter.sendMail(mailOptions);
-        res.status(200).json({ success: true, message: 'Email envoyé avec succès !' });
-    } catch (error) {
-        console.error('Erreur d’envoi :', error);
-        res.status(500).json({ success: false, message: 'Échec de l’envoi de l’e-mail.' });
-    }
+	try {
+		await transporter.sendMail(mailOptions);
+		res.status(200).json({ success: true, message: 'Email envoyé avec succès !' });
+	} catch (error) {
+		console.error('Erreur d’envoi :', error);
+		res.status(500).json({ success: false, message: 'Échec de l’envoi de l’e-mail.' });
+	}
 });
 
 module.exports = router;
