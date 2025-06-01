e41290321dcf8965445aca2ecbc3f6b016e76a14
farshed <faisalarshed28@gmail.com>
2025-06-01 05:10:56 +0500

    Update

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use('/story', storyRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/contactus', emailRoutes);
 
+app.get('/ping', (_, res) => res.type('text').send('pong'));
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
